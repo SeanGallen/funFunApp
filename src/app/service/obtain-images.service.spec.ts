0ee74a1c5322dfda3ba874fd86e02eacbdeaa151
Sean <sjgallen@gmail.com>
2019-11-09 00:11:01 -0600

    improved the test

--- src/app/service/obtain-images.service.spec.ts
+++ src/app/service/obtain-images.service.spec.ts
@@ -1,12 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ObtainImagesService } from './obtain-images.service';
 
-describe('ObtainImagesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+let httpTestingController: HttpTestingController;
+let service: ObtainImagesService; 
 
+describe('ObtainImagesService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ObtainImagesService],
+      imports: [HttpClientTestingModule]
+    });
+
+    service =  TestBed.get(ObtainImagesService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+  
   it('should be created', () => {
-    const service: ObtainImagesService = TestBed.get(ObtainImagesService);
     expect(service).toBeTruthy();
+  });
+  
+  it('should return JSON', () => {
   });
 });
