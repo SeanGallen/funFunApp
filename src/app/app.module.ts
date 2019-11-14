b1edfcab6f31fb28fb363fb9eea94a0fe4e4cd86
Sean <sjgallen@gmail.com>
2019-11-13 18:50:56 -0600

    adding new component for images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './component/app-component/app.component';
+import { ImagesComponent } from './component/images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
