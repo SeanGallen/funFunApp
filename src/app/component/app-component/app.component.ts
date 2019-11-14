f086aea46fd2cbc359c77971364ea12caf976b60
Sean <sjgallen@gmail.com>
2019-11-13 19:13:36 -0600

    extract component out of original app-component

--- src/app/component/app-component/app.component.ts
+++ src/app/component/app-component/app.component.ts
@@ -9,23 +9,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'funFunApp';
-  js : void = this.get();
-  jsonReturn : string;
-  image: string = '<br>'
+   title = 'funFunApp';
+   constructor( ) {}
 
-  constructor(private imageService : ObtainImagesService) {}
-
-  get(){
-    this.imageService.getAnImage().subscribe((c) => {
-      console.log(c)
-      this.jsonReturn = c.mission_name;
-      let images = c.links.flickr_images;
-      console.log(c.links.flickr_images[0]);
-      images.forEach(image => {
-        this.image += `<img width="400" height="400" src=${image}><br>`;
-      });
-    });
-  }
- 
 }
--- src/app/component/images/images.component.ts
+++ src/app/component/images/images.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ObtainImagesService } from './../../service/obtain-images.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
   styleUrls: ['./images.component.css']
 })
-export class ImagesComponent implements OnInit {
+export class ImagesComponent {
+  title = 'funFunApp';
+  js : void = this.get();
+  jsonReturn : string;
+  image: string = '<br>'
 
-  constructor() { }
+  constructor(private imageService : ObtainImagesService) {}
 
-  ngOnInit() {
+  get(){
+    this.imageService.getAnImage().subscribe((c) => {
+      console.log(c)
+      this.jsonReturn = c.mission_name;
+      let images = c.links.flickr_images;
+      console.log(c.links.flickr_images[0]);
+      images.forEach(image => {
+        this.image += `<img width="400" height="400" src=${image}><br>`;
+      });
+    });
   }
 
 }
